fe2f31ad4d51cd2d7cbaa3cf1d2a7b4719691f2b
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-06-15 10:41:35 +0200

    updated spatial data scripts

---
+++ data/party.sql
@@ -0,0 +1,39 @@
+﻿--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.2.3
+-- Dumped by pg_dump version 9.3.1
+-- Started on 2015-06-10 17:35:33
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = party, pg_catalog;
+
+--
+-- TOC entry 3880 (class 0 OID 597319)
+-- Dependencies: 226
+-- Data for Name: party; Type: TABLE DATA; Schema: party; Owner: postgres
+--
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE party DISABLE TRIGGER ALL;
+DELETE FROM party;
+
+INSERT INTO party (id, ext_id, type_code, name, last_name, fathers_name, fathers_last_name, alias, gender_code, address_id, id_type_code, id_number, email, mobile, phone, fax, preferred_communication_code, rowidentifier, rowversion, change_action, change_user, change_time, dob, state, nationality) VALUES ('ab43c852-1c7b-4ca5-9e88-b74a75d08ce1', NULL, 'naturalPerson', 'john', 'smith', NULL, NULL, NULL, 'male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '4aa04e02-6422-4e5f-9c2e-1ce6c6e44682', 1, 'i', 'test', '2015-06-10 17:32:09.344', '1976-06-02', NULL, 'Nigeria');
+INSERT INTO party (id, ext_id, type_code, name, last_name, fathers_name, fathers_last_name, alias, gender_code, address_id, id_type_code, id_number, email, mobile, phone, fax, preferred_communication_code, rowidentifier, rowversion, change_action, change_user, change_time, dob, state, nationality) VALUES ('3bb1a694-f077-4495-af5e-374adef4609d', NULL, 'naturalPerson', 'Maggie', 'Joansson', NULL, NULL, NULL, 'female', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'af2249c2-e57d-4cc5-b017-3d6af9f43675', 1, 'i', 'test', '2015-06-10 17:33:12.146', '1977-10-22', NULL, 'Anambra');
+
+
+ALTER TABLE party ENABLE TRIGGER ALL;
+
+-- Completed on 2015-06-10 17:35:33
+
+--
+-- PostgreSQL database dump complete
+--
+
--- extension/spatial_config.sql
+++ extension/spatial_config.sql
@@ -1,8 +1,8 @@
-﻿   
+   
 --- This is for correctly setting up the orthophoto onto localhost
 UPDATE system.config_map_layer 
 SET url = 'http://localhost:8085/geoserver/anambra/wms',
-wms_layers= 'anambra:Anambra',
+wms_layers= 'anambra:awka',
 wms_format= 'image/jpeg',
 visible_in_start = TRUE,
 active = TRUE
@@ -11,7 +11,7 @@ WHERE name='orthophoto';
 --- This is for correctly setting up the orthophoto onto anambra land ministry server
 --UPDATE system.config_map_layer 
 --SET url = 'http://192.168.0.6:8085/geoserver/anambra/wms',
---wms_layers= 'anambra:Anambra',
+--wms_layers= 'anambra:awka',
 --wms_format= 'image/jpeg',
 --visible_in_start = TRUE,
 --active = TRUE
@@ -234,9 +234,100 @@ insert into system.map_search_option(code, title, query_name, active, min_search
 ---- update for map label
 CREATE OR REPLACE FUNCTION cadastre.get_map_center_label(center_point geometry)
 RETURNS character varying AS $BODY$ begin
-return coalesce((select 'Section:' || label from cadastre.spatial_unit_group
+return coalesce((select 'Public Display Area:' || label from cadastre.spatial_unit_group
 where hierarchy_level = 4 and st_within(center_point, geom) limit 1), ''); end;
 $BODY$ LANGUAGE plpgsql;
+
+
+-- View: application.systematic_registration_certificates
+
+-- DROP VIEW application.systematic_registration_certificates;
+
+CREATE OR REPLACE VIEW application.systematic_registration_certificates AS 
+ SELECT DISTINCT aa.nr, co.name_firstpart, co.name_lastpart, su.ba_unit_id, 
+    sg.name::text AS name, aa.id::text AS appid, 
+    aa.change_time AS commencingdate, 
+    "substring"(lu.display_value::text, 0, "position"(lu.display_value::text, '-'::text)) AS landuse, 
+    ( SELECT lga.label
+           FROM cadastre.spatial_unit_group lga
+          WHERE lga.hierarchy_level = 2 AND co.name_lastpart::text ~~ (lga.name::text || '/%'::text)) AS proplocation, 
+    round(sa.size) AS size, 
+    administrative.get_parcel_share(su.ba_unit_id) AS owners, 
+    (co.name_lastpart::text || '/'::text) || upper(co.name_firstpart::text) AS title, 
+    co.id, 
+    ( SELECT lga.label
+           FROM cadastre.spatial_unit_group lga
+          WHERE lga.hierarchy_level = 3 AND co.name_lastpart::text = lga.name::text) AS ward, 
+    ( SELECT lga.label
+           FROM cadastre.spatial_unit_group lga
+          WHERE lga.hierarchy_level = 1) AS state, 
+    ( SELECT config_map_layer_metadata.value
+           FROM system.config_map_layer_metadata
+          WHERE config_map_layer_metadata.name_layer::text = 'orthophoto'::text AND config_map_layer_metadata.name::text = 'date'::text) AS imagerydate, 
+    (( SELECT count(s.id) AS count
+           FROM source.source s
+          WHERE s.description::text ~~ ((('TOTAL_'::text || 'title'::text) || '%'::text) || replace(sg.name::text, '/'::text, '-'::text))))::integer AS cofo, 
+    ( SELECT config_map_layer_metadata.value
+           FROM system.config_map_layer_metadata
+          WHERE config_map_layer_metadata.name_layer::text = 'orthophoto'::text AND config_map_layer_metadata.name::text = 'resolution'::text) AS imageryresolution, 
+    ( SELECT config_map_layer_metadata.value
+           FROM system.config_map_layer_metadata
+          WHERE config_map_layer_metadata.name_layer::text = 'orthophoto'::text AND config_map_layer_metadata.name::text = 'data-source'::text) AS imagerysource, 
+    ( SELECT config_map_layer_metadata.value
+           FROM system.config_map_layer_metadata
+          WHERE config_map_layer_metadata.name_layer::text = 'orthophoto'::text AND config_map_layer_metadata.name::text = 'sheet-number'::text) AS sheetnr, 
+    ( SELECT setting.vl
+           FROM system.setting
+          WHERE setting.name::text = 'surveyor'::text) AS surveyor, 
+    ( SELECT setting.vl
+           FROM system.setting
+          WHERE setting.name::text = 'surveyorRank'::text) AS rank
+   FROM cadastre.spatial_unit_group sg, cadastre.cadastre_object co, 
+    administrative.ba_unit bu, cadastre.land_use_type lu, 
+    cadastre.spatial_value_area sa, 
+    administrative.ba_unit_contains_spatial_unit su, 
+    application.application_property ap, application.application aa, 
+    application.service s
+  WHERE sg.hierarchy_level = 5 AND st_intersects(st_pointonsurface(co.geom_polygon), sg.geom) AND (co.name_firstpart::text || co.name_lastpart::text) = (ap.name_firstpart::text || ap.name_lastpart::text) AND (co.name_firstpart::text || co.name_lastpart::text) = (bu.name_firstpart::text || bu.name_lastpart::text) AND aa.id::text = ap.application_id::text AND s.application_id::text = aa.id::text AND s.request_type_code::text = 'systematicRegn'::text AND (aa.status_code::text = 'approved'::text OR aa.status_code::text = 'archived'::text) AND bu.id::text = su.ba_unit_id::text AND su.spatial_unit_id::text = sa.spatial_unit_id::text AND sa.spatial_unit_id::text = co.id::text AND sa.type_code::text = 'officialArea'::text AND COALESCE(bu.land_use_code, 'res_home'::character varying)::text = lu.code::text
+  ORDER BY co.name_firstpart, co.name_lastpart;
+
+  
+  -- Creates wrapper functions for those PostGIS 1.5
+ -- functions used by SOLA that have been deprecated
+ -- or removed from PostGIS 2.0
+
+
+    CREATE OR REPLACE FUNCTION public.st_3dmakebox (
+     geom1 public.geometry,
+     geom2 public.geometry
+    )
+    RETURNS public.box3d AS
+    '$libdir/postgis-2.0', 'BOX3D_construct'
+    LANGUAGE 'c'
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT
+    SECURITY INVOKER
+    COST 1;
+
+    COMMENT ON FUNCTION public.st_3dmakebox(geom1 public.geometry, geom2 public.geometry)
+    IS 'args: point3DLowLeftBottom, point3DUpRightTop - Creates a BOX3D defined by the given 3d point geometries.'; 
+
+
+
+
+ 
+ CREATE OR REPLACE FUNCTION ST_MakeBox3D(geometry, geometry) RETURNS 
+ box3d AS 'SELECT ST_3DMakeBox($1, $2)'
+ LANGUAGE 'sql' IMMUTABLE STRICT;
+
+ CREATE OR REPLACE FUNCTION SetSrid(geometry, integer) RETURNS 
+ geometry AS 'SELECT ST_SetSrid($1, $2)'
+ LANGUAGE 'sql' IMMUTABLE STRICT;
+
+
+
+
+
 
 
 -------------------------------------------- 
@@ -251,8 +342,10 @@ UPDATE system.setting SET vl = '756931' WHERE "name" = 'map-north';
 UPDATE system.crs SET srid = '32632';
 
 ---------------update cadastre.hierarchy_level --------------------------
-update cadastre.hierarchy_level set status = 'x' where code in ('0','1','2','3');
-
+update cadastre.hierarchy_level set status = 'x' where code in ('0','1','2','3'); -- disable spatial unit editor for nation, state, lga, ward
+--update cadastre.hierarchy_level set status = 'c' where code = '2'; -- enable spatial unit editor for lgas
+update cadastre.hierarchy_level set status = 'c' where code = '3'; -- enable spatial unit editor for wards
+ 
 
 
 
