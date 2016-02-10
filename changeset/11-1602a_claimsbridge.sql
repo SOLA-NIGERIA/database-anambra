2b244439e40f47c01829ff1105ccaf57135975a2
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2016-02-10 14:03:44 +0100

    changed icon for the taskbar

---
+++ changeset/11-1602a_claimsbridge.sql
@@ -0,0 +1,11 @@
+﻿
+INSERT INTO source.administrative_source_type(
+            code, display_value, status, description, is_for_registration)
+    VALUES ('claimForm', 'OT Claim Summary', 'c', 'Extension to LADM to link opentenure claims', false);
+
+
+INSERT INTO application.request_type_requires_source_type(
+            source_type_code, request_type_code)
+    VALUES ('claimForm', 'systematicRegn');
+
+
