dd2c70f9b49ad7b03114cfa02939437913fd87f6
unknown <rizzom@NRC016.hq.un.fao.org>
2016-08-23 16:56:45 +0200

    renewed cacert and keystore

---
+++ changeset/22-1608b_LH76.sql
@@ -0,0 +1,4 @@
+﻿update system.config_map_layer set active = false, visible_in_start = false where name = 'sug_hierarchy';
+
+
+update system.config_map_layer set active = false, visible_in_start = false where name = 'road-centerlines-for-parcel-plan';
