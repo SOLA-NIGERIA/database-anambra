86031ca317db55e91e3eb7bf31eded6f677e0865
unknown <rizzom@NRC016.hq.un.fao.org>
2016-10-28 18:26:07 +0200

    fixed tickets 75,81,85,86,87,88,89,90

--- changeset/34-LH86-1610a.sql
+++ changeset/34-LH86-1610a.sql
@@ -1,4 +1,58 @@
-﻿--Add BR to be applied on approval of SLTR Claim Application that checks that the following fields are populated : Estate, Area, Purpose, Date Commenced, term
+--Add BR to be applied on approval of SLTR Claim Application that checks that the following fields are populated : Estate, Area, Purpose, Date Commenced, term
+INSERT INTO system.version SELECT '1610b' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1610b');
+
+
+ 
+-- Function: administrative.get_improvementPremium(character varying)
+--select administrative.get_improvementPremium('8be78a84-3eb8-4968-a259-910bddc29c54');
+DROP FUNCTION administrative.get_improvementPremium(character varying);
+  
+CREATE OR REPLACE FUNCTION administrative.get_improvementPremium(inputid character varying)
+  RETURNS numeric AS
+$BODY$
+declare
+  rec record;
+  returnValue numeric;
+  --character varying ;
+  --integer;
+   
+BEGIN
+
+returnValue = 0;
+
+ 
+select
+ CASE 	WHEN (rrr.rot_code = 'P') THEN ltr.premium_non_state_land
+	WHEN (rrr.rot_code = 'G') THEN ltr.premium_state_land
+		ELSE  	0
+ END 	 										      
+into returnValue
+FROM
+ cadastre.lga_tarrif_rate ltr,  
+ administrative.rrr rrr,
+ administrative.ba_unit bu, 
+ cadastre.land_use_type lu
+WHERE
+bu.id::text = rrr.ba_unit_id::text
+AND COALESCE(bu.land_use_code, 'res_home'::character varying)::text = lu.code::text
+AND lu.tarrif_code = ltr.tarrif_type
+AND is_primary
+AND ltr.sug_id = "substring"(bu.name_lastpart::text,0, "position"("substring"(bu.name_lastpart::text, "position"(bu.name_lastpart::text, '/'::text)+1), '/'::text)+"position"(bu.name_lastpart::text, '/'::text))
+AND rrr.ba_unit_id = inputid;
+
+-- UPDATE administrative.rrr
+-- SET improvement_premium = returnValue
+-- WHERE ba_unit_id = inputid;
+ 
+	
+return returnValue;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION administrative.get_improvementPremium(character varying)
+  OWNER TO postgres;
+
 
 --- stamp duty
 -- Function: administrative.get_stampDuty(character varying)
@@ -10,16 +64,16 @@ $BODY$
 declare
   rec record;
   returnValue numeric ;
-  --integer;
+  percstampDuty numeric ;
    
 BEGIN
 
 returnValue = 0;
- 											
+select cast(vl as numeric) into percstampDuty from system.setting where name =  'percStampDuty' 	;										
  
 select
- CASE 	WHEN (rrr.rot_code = 'P') THEN ltr.rent_non_state_land * round(sa.size)
-	WHEN (rrr.rot_code = 'G') THEN ltr.rent_state_land *round(sa.size)
+ CASE 	WHEN (rrr.rot_code = 'P') THEN ltr.premium_non_state_land * round(sa.size) * percstampDuty
+	WHEN (rrr.rot_code = 'G') THEN ltr.premium_state_land *round(sa.size) * percstampDuty
 		ELSE  	0
  END 	 										      
 into returnValue
@@ -43,7 +97,10 @@ AND ltr.sug_id = "substring"(bu.name_lastpart::text,0, "position"("substring"(bu
 AND sa.type_code::text = 'officialArea'::text 
 AND rrr.ba_unit_id = inputid;
 
-
+---UPDATE administrative.rrr
+ --SET stamp_duty = returnValue
+-- WHERE ba_unit_id = inputid;
+ 
 	
 return returnValue;
 END;
@@ -110,7 +167,10 @@ AND ltr.sug_id = "substring"(bu.name_lastpart::text,0, "position"("substring"(bu
 AND sa.type_code::text = 'officialArea'::text 
 AND rrr.ba_unit_id = inputid;
 
-
+--UPDATE administrative.rrr
+-- SET yearly_rent = returnValue
+-- WHERE ba_unit_id = inputid;
+ 
 	
 return returnValue;
 END;
